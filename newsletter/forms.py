6c9ff012dd1daef4a0e67210ae77528d5947c3ec
root <root@ip-172-31-46-187.us-east-2.compute.internal>
2021-06-11 11:54:23 +0000

    missed 1 file

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,17 +1,23 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField, PasswordField, BooleanField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from . models import Article_category
+
 
 def length_check(form,field):
     if len(field.data) == 0:
         raise ValidationError('Fields should not be null')
     
+def choice_query():
+    return Article_category.query
 
-class AddArticlesForm(Form):
+class AddArticlesForm(FlaskForm):
     url= TextField('url', validators= [DataRequired()])
     title = TextField('title', validators= [DataRequired()])
     description = TextField('description', validators= [ DataRequired(), Length(min=4)])
-    time = TextField('time',validators=[ DataRequired(), Length(min=6)])
-    category_id = SelectField('category', validators= [DataRequired()],choices =[])
-    submit = SubmitField('Sign Up')
+    time = TextField('time',validators=[ DataRequired()])
+#    category_id = SelectField('category',id ='category_id')
+    category_id = QuerySelectField(query_factory=choice_query, allow_blank=True,get_label='category_name')
+    submit = SubmitField('Add Article')
