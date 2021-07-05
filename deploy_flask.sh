223bb628babe1e62ec73dcf00d1f62caedbc035d
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-05 08:48:16 +0530

    Automate deploy of flask app

---
+++ deploy_flask.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Script to deploy flask app to EC2 instance
+cd /home/ubuntu
+pip3 install virtualenv
+if [ -d "venv_newsletter" ]
+then
+        echo "Virtual environment already exists"
+else
+        echo "Virtual environment does not exists"
+        virtualenv venv_newsletter
+fi
+source venv_newsletter/bin/activate
+pip install -r requirements.txt
+cp -R newsletter_automation-0.0.2/newsletter_automation venv_newsletter/lib/python3*/site-packages
+cd venv_newsletter/lib/python3*/site-packages/newsletter_automation
+ps -ef | grep python  | grep run.py | awk {'print $2'} | xargs kill -9 2>/dev/null
+nohup python3 run.py 2>/home/ubuntu/nohup.err 1>/home/ubuntu/nohup.out &
+
