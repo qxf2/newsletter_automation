831bd0a26d4124dafd540f2841c38f0a9d311911
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-05 19:23:28 +0530

    Run flask app

--- deploy_flask.sh
+++ deploy_flask.sh
@@ -13,7 +13,7 @@ source venv_newsletter/bin/activate
 pip install -r requirements.txt
 cp -R newsletter_automation-0.0.2/newsletter_automation venv_newsletter/lib/python3*/site-packages
 cd venv_newsletter/lib/python3*/site-packages/newsletter_automation
-ps -ef | grep python  | grep run.py | awk {'print $2'} | xargs kill -9 2>/dev/null
+ps -ef | grep python  | grep flask | awk {'print $2'} | xargs kill -9 2>/dev/null
 export FLASK_APP=run
-nohup flask run --host=0.0.0.0 --port=5000 2>/home/ubuntu/nohup.err 1>/home/ubuntu/nohup.out &
+nohup flask run --host=0.0.0.0 --port=8080 2>/home/ubuntu/nohup.err 1>/home/ubuntu/nohup.out &
 
