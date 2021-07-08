5435865fe21ecff7d598e3c430379074d3c099f3
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-08 10:42:39 +0530

    Test curl command in automate workflow

--- deploy_flask.sh
+++ deploy_flask.sh
@@ -2,18 +2,22 @@
 #Script to deploy flask app to EC2 instance
 cd /home/ubuntu
 pip3 install virtualenv
+#check if the virtualenvironment exists
 if [ -d "venv_newsletter" ]
 then
         echo "Virtual environment already exists"
 else
-        echo "Virtual environment does not exists"
+        echo "Virtual environment does not exist"
         virtualenv venv_newsletter
 fi
 source venv_newsletter/bin/activate
 pip install -r requirements.txt
+#place the newsletter folder inside the virutalenv. Will not be required once it is available as pip installable
 cp -R newsletter_automation-0.0.2/newsletter_automation venv_newsletter/lib/python3*/site-packages
 cd venv_newsletter/lib/python3*/site-packages/newsletter_automation
+#terminate existing flask process
 ps -ef | grep python  | grep flask | awk {'print $2'} | xargs kill -9 2>/dev/null
+#deploy the flask app
 export FLASK_APP=run
 nohup flask run --host=0.0.0.0 --port=8080 2>/home/ubuntu/nohup.err 1>/home/ubuntu/nohup.out &
 
