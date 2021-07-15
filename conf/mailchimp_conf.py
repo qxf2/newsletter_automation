2ff0124f671929dd8c1b4be798f81e0bdf4ecda3
nelabhotlaR <47164371+nelabhotlaR@users.noreply.github.com>
2021-07-15 17:56:00 +0530

    Update mailchimp_conf.py

--- conf/mailchimp_conf.py
+++ conf/mailchimp_conf.py
@@ -1,8 +1,8 @@
 """
 Configuration for mailchimp integrtaion
 """
-MAILCHIMP_API_KEY='754e773e384aeb594a6734e401537dcc-us1'
-SERVER_PREFIX = 'us1'
-SUBSCRIBER_LIST_ID = '2ee66398bd'
+MAILCHIMP_API_KEY=''
+SERVER_PREFIX = ''
+SUBSCRIBER_LIST_ID = ''
 FROM_NAME= 'Qxf2 Services'
 REPLY_TO='informedtester@qxf2.com'
