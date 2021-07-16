939310f9d3134835f4729bd9ec9c3e4dc2f702f4
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-16 15:46:37 +0530

    Added setup file for building wheel package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='newsletter-automation',
+      version='0.1-1',
+      description='Newsletter Automation Project',
+      maintainer='Qxf2 Services',
+      maintainer_email='test@qxf2.com',
+      url='https://github.com/qxf2/newsletter_automation'
+     )
