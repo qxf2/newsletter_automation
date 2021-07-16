ed3cd524890de369f0982ad902c98ba015d59ea0
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-16 23:46:37 +0530

    Changed directory structure and added necessary files for creating python package

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+setuptools.setup(
+    name="newsletter-automation",
+    version="0.1.1",
+    description="Newsletter Automation Project",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    maintainer='Qxf2 Services',
+    maintainer_email='test@qxf2.com',
+    url='https://github.com/qxf2/newsletter_automation',
+    packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.txt","*.html","*.py"]}
+)
 
-setup(name='newsletter-automation',
-      version='0.1-1',
-      description='Newsletter Automation Project',
-      maintainer='Qxf2 Services',
-      maintainer_email='test@qxf2.com',
-      url='https://github.com/qxf2/newsletter_automation'
-     )
