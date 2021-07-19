e800a7974a5c9d1ba5c82477ee95374faf404058
shivahari <shivahari@qxf2.com>
2021-07-19 20:51:28 +0530

    Removed steps to use ENV variables for username and passoword for DB

--- newsletter_automation/newsletter/__init__.py
+++ newsletter_automation/newsletter/__init__.py
@@ -3,11 +3,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-USERNAME = os.getenv('MYSQL_USERNAME')
-PASSWORD = os.getenv('MYSQL_PASSWORD')
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%s:%s@127.0.0.1/newsletter_automation' % (USERNAME, PASSWORD)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/newsletter_automation'
 app.config['SECRET_KEY'] = "random string"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_object(__name__)
