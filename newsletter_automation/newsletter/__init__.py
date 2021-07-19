47fab9b28851795f1f8e73b7a802d29e258adfb0
shivahari <shivahari@qxf2.com>
2021-07-19 12:19:48 +0530

    Modified the DB module to use the username & password env variables

--- newsletter_automation/newsletter/__init__.py
+++ newsletter_automation/newsletter/__init__.py
@@ -1,16 +1,18 @@
+#!/bin/env python
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+USERNAME = os.getenv('MYSQL_USERNAME')
+PASSWORD = os.getenv('MYSQL_PASSWORD')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/newsletter_automation'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%s:%s@127.0.0.1/newsletter_automation' % (USERNAME, PASSWORD)
 app.config['SECRET_KEY'] = "random string"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
-
 
 from newsletter import models
 from newsletter import routes
