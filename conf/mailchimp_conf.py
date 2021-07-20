8f369544c4aebfcced69e58fa3562ba0ada35a1c
PreedhiVivek <preedhi.vivek@qxf2.com>
2021-07-20 14:32:09 +0530

    Updated with env vars.

--- conf/mailchimp_conf.py
+++ conf/mailchimp_conf.py
@@ -1,8 +1,9 @@
 """
 Configuration for mailchimp integrtaion
 """
-MAILCHIMP_API_KEY=''
-SERVER_PREFIX = ''
-SUBSCRIBER_LIST_ID = ''
+import os
+MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
+SERVER_PREFIX = os.environ.get('SERVER_PREFIX')
+SUBSCRIBER_LIST_ID = os.environ.get('SUBSCRIBER_LIST_ID')
 FROM_NAME= 'Qxf2 Services'
 REPLY_TO='informedtester@qxf2.com'
