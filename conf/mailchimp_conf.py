35e63f4445da717194fc9dc81401febe073f4da7
Rohini Gopal <rohini.gopal@qxf2.com>
2021-07-20 15:03:30 +0530

    reverting the config values which got pushed

--- conf/mailchimp_conf.py
+++ conf/mailchimp_conf.py
@@ -1,8 +1,8 @@
 """
 Configuration for mailchimp integrtaion
 """
-MAILCHIMP_API_KEY='3d15308cc616f397df542ad166778440-us1'
-SERVER_PREFIX = 'us1'
-SUBSCRIBER_LIST_ID = '2ee66398bd'
+MAILCHIMP_API_KEY=''
+SERVER_PREFIX = ''
+SUBSCRIBER_LIST_ID = ''
 FROM_NAME= 'Qxf2 Services'
 REPLY_TO='informedtester@qxf2.com'
