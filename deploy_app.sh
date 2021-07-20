eb7a7936e78686a346d50ec175135f8fd3e8277b
shivahari <shivahari@qxf2.com>
2021-07-20 14:36:33 +0530

    Removed redundant deploy script

--- deploy_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-sudo systemctl stop newsletter_automation_app
-sudo source /home/ubuntu/venv/newsletter_app/bin/activate
-cd /home/ubuntu/
-sudo python -m pip install *.tar.gz
-sudo systemctl start newsletter_automation_app
