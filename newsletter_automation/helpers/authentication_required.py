31a8262bac430014f7f31b718f68604af5a10121
mohanqxf2 <37790403+mohanqxf2@users.noreply.github.com>
2021-08-10 13:29:06 +0530

    Revert "Revert "api authentication (#80)" (#84)" (#85)
    This reverts commit ade87111b0ea3fd124c21dd3c505f8845ddcfa59.

--- newsletter_automation/helpers/authentication_required.py
+++ newsletter_automation/helpers/authentication_required.py
@@ -3,7 +3,9 @@ Decorator for authenticating all pages
 """
 
 from functools import wraps
-from flask import render_template, session
+from flask import render_template, session, request, jsonify
+import conf.apikey_conf as APIKEY
+
 class Authentication_Required:
     "Authentication for all classes"
     def requires_auth(func):
@@ -19,3 +21,23 @@ class Authentication_Required:
                 return render_template("unauthorized.html")
 
         return decorated
+    
+    def requires_apikey(func):
+        " Decorator function to require API Key "
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            " Decorator function that does the checking "
+            if request.headers.get('x-api-key') and request.headers.get('X-API-KEY') == APIKEY.API_KEY:
+                return func(*args, **kwargs)
+            else:
+                return authenticate_error(func)
+        return decorated
+
+def authenticate_error(auth_flag):
+    "set authentication message "
+    message = {'message': "API key is not valid"}
+    response = jsonify(message)
+    response.status_code = 401
+    response.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+
+    return response
