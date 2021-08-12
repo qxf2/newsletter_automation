4c444f1de946727dcf1746665df5b60a2aca580b
Raji Gali <rajeswari.gali@sciencelogic.com>
2021-08-12 16:31:48 +0530

    Adding unit tests for DB integration

---
+++ newsletter_automation/tests/db_tests/db_tests_models.py
@@ -0,0 +1,95 @@
+"""
+This file has been created for testing purpose which contains expected Models to compare with the actual data base schemas
+"""
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from newsletter import db
+
+class AddNewsletter(db.Model):
+    "Class for AddNewsletter db model"
+    __tablename__ = 'test_add_newsletter'
+    __bind_key__ = 'qa_db'
+    newsletter_id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(50))
+    opener = db.Column(db.String(400))
+    preview = db.Column(db.String(400))
+    #campaign_id= db.Column(db.Integer)
+
+    def __init__(self, subject, opener,preview):
+        "initialize subject,opener"
+        self.subject = subject
+        self.opener = opener
+        self.preview = preview
+
+class NewsletterContent(db.Model):
+    "Class for NewsletterContent db model"
+    __tablename__ = 'test_newsletter_content'
+    __bind_key__ = 'qa_db'
+    newsletter_content_id = db.Column(db.Integer, primary_key=True)
+    newsletter_id = db.Column(db.Integer, db.ForeignKey('test_add_newsletter.newsletter_id'))
+    article_id = db.Column(db.Integer, db.ForeignKey('test_articles.article_id'))
+
+    def __init__(self, newsletter_id,  article_id):
+        self.newsletter_id = newsletter_id
+        self.article_id = article_id
+
+class Articles(db.Model):
+    "Class for Article db model"
+    __tablename__ = 'test_articles'
+    __bind_key__ = 'qa_db'
+    article_id = db.Column('article_id', db.Integer, primary_key = True)
+    url = db.Column(db.String(100))
+    title = db.Column(db.String(250))
+    description = db.Column(db.String(500))
+    time = db.Column(db.String(300))
+    category_id = db.Column(db.Integer, db.ForeignKey('test_article_category.category_id'))
+    newsletter_id = db.Column(db.Integer, db.ForeignKey('test_add_newsletter.newsletter_id'))
+
+    def __init__(self, url, title, description, time,category_id):
+        "initializes url, title, description, time, category_id"
+        self.url = url
+        self.title = title
+        self.description = description
+        self.time = time
+        self.category_id = category_id
+
+
+class Article_category(db.Model):
+    "Class for Article_category db model"
+    __tablename__ = 'test_article_category'
+    __bind_key__ = 'qa_db'
+    category_id = db.Column('category_id', db.Integer, primary_key = True)
+    category_name = db.Column(db.String(100))
+
+    def __init__(self, category_name):
+        "initializes category_name"
+        self.category_name = category_name
+
+
+class Newsletter_schedule(db.Model):
+    "Newsletter schedule table"
+    __tablename__ = 'test_newsletter_schedule'
+    __bind_key__ = 'qa_db'
+    schedule_id = db.Column('schedule_id', db.Integer, primary_key=True)
+    newsletter_id = db.Column('newsletter_id',db.Integer,db.ForeignKey('test_add_newsletter.newsletter_id'))
+    schedule_date = db.Column('schedule_date',db.Integer)
+
+    def __init__(self,schedule_id,newsletter_id,schedule_date):
+        self.schedule_id = schedule_id
+        self.newsletter_id = newsletter_id
+        self.schedule_date = schedule_date
+
+class Newsletter_campaign(db.Model):
+    "Newsletter create campaign table"
+    __tablename__ = 'test_newsletter_campaign'
+    __bind_key__ = 'qa_db'
+    Newsletter_campaign_id =db.Column('Newsletter_campaign_id', db.Integer, primary_key=True)
+    campaign_id = db.Column('campaign_id', db.String(50))
+    newsletter_id = db.Column('newsletter_id',db.Integer,db.ForeignKey('test_add_newsletter.newsletter_id'))
+
+    def __init__(self,campaign_id,newsletter_id):
+        self.campaign_id = campaign_id
+        self.newsletter_id = newsletter_id
+
+db.create_all()
+db.session.commit()
