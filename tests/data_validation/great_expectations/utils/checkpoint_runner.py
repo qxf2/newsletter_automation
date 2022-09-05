37053ff497f446bec3a278525ac37bf12dfdc386
shivaharip <shivahari@qxf2.com>
2022-09-05 21:52:03 +0530

    Added Checkpoints class to classify checkpoint based on execution time

--- tests/data_validation/great_expectations/utils/checkpoint_runner.py
+++ tests/data_validation/great_expectations/utils/checkpoint_runner.py
@@ -6,6 +6,12 @@ from great_expectations.checkpoint.types.checkpoint_result import CheckpointResu
 from great_expectations.data_context import DataContext
 
 
+class Checkpoints(Enum):
+    checkpoint10am = []
+    checkpoint11am = ['run_ge_tests_11am_checkpoint']
+    checkpoint3pm = []
+
+
 def run_checkpoint(datacontext: DataContext,
                    checkpoint_name : str,
                    batch_request=None,
@@ -17,10 +23,19 @@ def run_checkpoint(datacontext: DataContext,
                                        run_name)
 
 
-
 if __name__ == "__main__":
     parser = optparse.OptionParser()
     parser.add_option("--checkpoint",
                       dest="checkpoint_name",
+                      default=None,
                       type="string")
+    parser.print_help(f"Pass one of {[checkpoint.name where checkpoint in Checkpoints]} with the --checkpoint param")
+    (options, args) = parser.parse_args()
+    checkpoint_name = options.checkpoint_name
 
+    if checkpoint_name:
+        for checkpoint in Checkpoints[checkpoint_name].value:
+            run_checkpoint(checkpoint.datacontext,
+                           checkpoint.checkpoint_name,
+                           checkpoint.batch_request,
+                           checkpoint.run_name)
