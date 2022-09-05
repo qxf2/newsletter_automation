175df7fb18ea66296f9520dc9de82200d455cc09
shivaharip <shivahari@qxf2.com>
2022-09-05 21:50:06 +0530

    Modified the runner script to import checkpoint modules and run them based on the --checkpoint arg passed
    Added Checkpoints class to classify checkpoint based on execution time
    
    Fixed typo
    
    Added import statement to import the ge test module
    
    Replaced the optparse module with argparse

--- tests/data_validation/great_expectations/utils/checkpoint_runner.py
+++ tests/data_validation/great_expectations/utils/checkpoint_runner.py
@@ -1,26 +1,44 @@
 #!/bin/bash/env python3
 
-import optparse
+import argparse
 from enum import Enum
 from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 from great_expectations.data_context import DataContext
 
+import run_ge_tests_11am_checkpoint
 
-def run_checkpoint(datacontext: DataContext,
+
+class Checkpoints(Enum):
+    checkpoint10am = []
+    checkpoint11am = [run_ge_tests_11am_checkpoint]
+    checkpoint3pm = []
+
+
+def run_checkpoint(data_context: DataContext,
                    checkpoint_name : str,
                    batch_request=None,
                    run_name=None) -> CheckpointResult:
     "Run checkpoint"
 
-    return data_context.run_checkpoint(checkpoint_name,
+    result = data_context.run_checkpoint(checkpoint_name,
                                        batch_request,
                                        run_name)
-
+    if not result['success']:
+        print('Validation Failed!')
 
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option("--checkpoint",
-                      dest="checkpoint_name",
-                      type="string")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint",
+                        dest="checkpoint_name",
+                        default=None,
+                        help =f"Pass one of {[checkpoint.name for checkpoint in Checkpoints]} with the --checkpoint param")
+    args = parser.parse_args()
+    checkpoint_name = args.checkpoint_name
 
+    if checkpoint_name:
+        for checkpoint in Checkpoints[checkpoint_name].value:
+            run_checkpoint(checkpoint.data_context,
+                           checkpoint.checkpoint_name,
+                           checkpoint.batch_request,
+                           checkpoint.run_name)
