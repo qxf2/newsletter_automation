36772d7c2998b71ae9deb6773164700fe1e70fc5
Qxf2 <mak@qxf2.com>
2022-09-11 18:43:40 +0530

    Updated the test to assert and handle https

--- tests/test_ruby_api_key_dynamic.rb
+++ tests/test_ruby_api_key_dynamic.rb
@@ -1,12 +1,14 @@
 
 require 'uri'
 require 'net/http'
-#require 'json'
+require 'json'
+require 'test/unit/assertions'
+include Test::Unit::Assertions
 
 apikey = ENV['API_KEY']
 
 class Newsletter
-  
+
   def set_base_uri(uri)
       @uri = uri
   end
@@ -25,28 +27,33 @@ end
 if !ARGV[0]
   uri = URI('http://127.0.0.1:5000/api/articles')
 else
-  uri = ARGV[0]
+  uri = URI(ARGV[0])
 end
-puts uri
 
+#Create a http object ... use SSL in case of https
+http = Net::HTTP.new(uri.host, uri.port)
+if uri.scheme == 'https'
+    http.use_ssl = true
+end
+
+#Setup the header and parameters
 site = Newsletter.new()
 site.set_base_uri(uri)
 locator = site.generate_code(10)
 url = 'https://qxf2.com/'+locator
 site.set_data_url(url)
 cid = rand 1..5
-
 header = {'x-api-key'=> apikey}
 req = Net::HTTP::Post.new(uri, header)
 req.set_form_data('url' => url, 'category_id' => cid, 'article_editor' => 'Ajitava')
 
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
+#Make the post
+res = http.request(req)
 
-case res
-when Net::HTTPSuccess, Net::HTTPRedirection
-  puts "Response #{res.code} #{res.message}: #{res.body}"
-else
-  res.value
-end
+#Parse the message
+message = JSON.parse(res.body)
+
+#Assert we have what we want
+assert_equal message['message'], "Record added successfully", "FAIL: API insert returned message #{message['message']}"
+assert_equal res.code, "200", "FAIL: API insert returned #{res.code} instead of 200"
+puts "API insert test PASSED"
