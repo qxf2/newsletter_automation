e3933b524c29c402d28f6db49dcd71a1757f1571
PreedhiVivek <preedhi.vivek@qxf2.com>
2022-09-28 15:12:54 +0530

    Updated environment variable name that stores Skype channel ID

--- tests/data_validation/great_expectations/conf/skype_conf.py
+++ tests/data_validation/great_expectations/conf/skype_conf.py
@@ -1,5 +1,5 @@
 import os
 
 SKYPE_URL = os.environ['SKYPE_URL']
-SKYPE_CHANNEL = os.environ['SKYPE_CHANNEL_TEST']
+SKYPE_CHANNEL = os.environ['SKYPE_CHANNEL']
 SKYPE_API_KEY = os.environ['SKYPE_API_KEY']
