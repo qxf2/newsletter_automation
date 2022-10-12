fe52813acffc82edc62761f1fc59a67b4f056f56
Qxf2 <mak@qxf2.com>
2022-10-12 21:35:28 +0530

    URL encoded the password in the connection string

--- newsletter_automation/newsletter/__init__.py
+++ newsletter_automation/newsletter/__init__.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 import os
+import urllib
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,7 +9,7 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 db_user = os.environ.get("MYSQL_USERNAME","root")
 db_password = os.environ.get("MYSQL_PASSWORD","root")
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@127.0.0.1/newsletter_automation'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{urllib.quote(db_password)}@127.0.0.1/newsletter_automation'
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET","random string")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_object(__name__)
