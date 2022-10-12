10f9cf2b87df0d01c0a36f4fcce6bad5bf4b7641
Qxf2 Services <mak@qxf2.com>
2022-10-12 18:42:54 +0530

    Added validators for the newsletter opener, subject and preview fields (#163)

--- newsletter_automation/newsletter/create_newsletter_form.py
+++ newsletter_automation/newsletter/create_newsletter_form.py
@@ -1,9 +1,12 @@
 """
 This Module Contains the Form classes for Add articles form
 """
+from email import message
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired, Length,ValidationError,url, Regexp
 from . models import Article_category
 
 def choice_query():
@@ -20,10 +23,9 @@ class ArticleForm(FlaskForm):
     description = TextAreaField('Description',render_kw ={'readonly':True})
     reading_time = StringField('Reading Time', render_kw ={'readonly':True})
     add_more = SubmitField('Add More Articles')
-    subject = StringField('Subject',render_kw={'placeholder': 'Just the date in dd-mmm-yyyy'})
+    subject = StringField('Subject',render_kw={'placeholder': 'Just the date in dd-mmm-yyyy'}, validators=[DataRequired(message="Subject cannot be blank"), Regexp('^\d\d-[A-Z][a-z][a-z]-\d\d\d\d', message='Invalid Subject. Subject should be of form dd-MMM-YYY E.g.: 15-Aug-1947'), Length(min=11, max=11, message='Subject should be exactly 11 characters long. Subject should be of form dd-MMM-YYY E.g.: 15-Aug-1947')])
     added_articles = my_field = TextAreaField('Added Articles:', render_kw={'readonly': True})
-    opener = TextAreaField('Opener',render_kw={'placeholder': 'Do NOT include the string "In this issue: "'})
-    preview_text = TextAreaField('Preview Text',render_kw={'maxlength': 150})
-    #schedule = SubmitField('Schedule')
+    opener = TextAreaField('Opener',render_kw={'placeholder': 'Do NOT include the string "In this issue: "'}, validators=[DataRequired(message="Opener cannot be blank"), Length(min=150, max=400, message='Opener should be between 150 and 399 characters long')])
+    preview_text = TextAreaField('Preview Text',render_kw={'maxlength': 150}, validators=[DataRequired(message="Preview text cannot be blank"), Length(min=70, max=150, message='Preview should be between 70 and 150 characters long')])
     preview_newsletter = SubmitField('Preview Newsletter')
     cancel = SubmitField('Clear Fields')
