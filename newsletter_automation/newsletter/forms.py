040488fb574717d20f15077f6dc8ae2d657d060a
Qxf2 <mak@qxf2.com>
2022-10-13 16:10:01 +0530

    Added a custom check for a URL without a domain

--- newsletter_automation/newsletter/forms.py
+++ newsletter_automation/newsletter/forms.py
@@ -7,6 +7,7 @@ from wtforms.fields.html5 import URLField
 from wtforms.fields.core import Label, StringField
 from wtforms.validators import DataRequired, Length,ValidationError,url, Regexp
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
+import tld
 from . models import Article_category
 
 #Check for fields not empty
@@ -18,9 +19,22 @@ def length_check(form,field):
 def choice_query():
     return Article_category.query
 
+#This is to make sure the URL has a domain between the protocol (or www) and the TLD
+def domain_present_check(form, field):
+    "Do not allow URLs like https://www.com"
+    url_tld = tld.get_tld(field.data, fail_silently=True)
+    if url_tld:
+        domain = field.data.split('://')[-1]
+        domain = domain.split('www.')[-1]
+        domain = domain.split(url_tld)[0]
+        if not domain.strip():
+            raise ValidationError('URL is missing a domain!')
+    else:
+        raise ValidationError('URL is missing tld')
+
 #To add articles data
 class AddArticlesForm(FlaskForm):
-    url = URLField('url', validators=[DataRequired(message="URL cannot be blank"), url(message='Invalid URL pattern'), Regexp('^(http|https)://', message='Invalid URL. URL should start with http(s)://'), Length(max=512, message='URL should be less than 512 characters')])
+    url = URLField('url', validators=[DataRequired(message="URL cannot be blank"), url(message='Invalid URL pattern'), Regexp('^(http|https)://', message='Invalid URL. URL should start with http(s)://'), Length(max=512, message='URL should be less than 512 characters'), domain_present_check])
     title = TextField('Title', validators=[DataRequired(message="Title cannot be blank"), Length(max=250, message='Title should be less than 250 characters')])
     description = TextAreaField('Description', validators=[DataRequired(message="Description cannot be blank"), Length(max=500, message='Description should be less than 500 characters')])
     time = TextField('Time', validators=[Regexp('^[1-9]\d*$', message='Time should be an integer greater than 0'), DataRequired(message="Time field cannot be blank")])
