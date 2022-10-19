c9dbc1b44503b00fff21df54e751f8a2dda12fc9
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-19 11:15:33 +0530

    UI test_automation

--- tests/data_validation/Integration/conf/manage_articles_conf.py
+++ tests/data_validation/Integration/conf/manage_articles_conf.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-email  = "aruul.bhagkya@qxf2.com"
-password = "AganSarwin@2021"
-=======
-email  = "aruul.bhagkya@qxf2.com"
-password = "AganSarwin@2021"
->>>>>>> 4e5cbd7825e7629c1aff3910a8313c0b3082fd0b
+email  = "Enter the username"
+password = "Enter the password"
 search="alerts"
