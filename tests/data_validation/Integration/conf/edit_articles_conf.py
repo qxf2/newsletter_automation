33e4c2177016218a9b8aac6611d48ba2f253f2a0
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-19 10:42:23 +0530

    UI test_automation

---
+++ tests/data_validation/Integration/conf/edit_articles_conf.py
@@ -0,0 +1,8 @@
+email  = "aruul.bhagkya@qxf2.com"
+password = "AganSarwin@2021"
+url="https://www.python.org/downloads/release/python-381/"
+title="testing selenium"
+description="python-selenium-change-url"
+runtime="15"
+category="currentweek"
+search="python"
