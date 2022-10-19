4ee89c7703be12391bab44f050c83ea4aa0ebad8
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-19 11:23:27 +0530

    UI test_automation

--- tests/data_validation/Integration/conf/edit_articles_conf.py
+++ tests/data_validation/Integration/conf/edit_articles_conf.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-email  = "aruul.bhagkya@qxf2.com"
-password = "AganSarwin@2021"
+email  = "Enter the username"
+password = "Enter the password"
 url="https://www.python.org/downloads/release/python-381/"
 title="testing selenium"
 description="python-selenium-change-url"
 runtime="15"
 category="currentweek"
 search="python"
-=======
-email  = "aruul.bhagkya@qxf2.com"
-password = "AganSarwin@2021"
-url="https://www.python.org/downloads/release/python-381/"
-title="testing selenium"
-description="python-selenium-change-url"
-runtime="15"
-category="currentweek"
-search="python"
->>>>>>> 4e5cbd7825e7629c1aff3910a8313c0b3082fd0b
