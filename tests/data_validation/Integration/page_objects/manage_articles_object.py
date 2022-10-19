1f95560a97d97fba234ebf17af29d540fa9636fc
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-19 11:40:57 +0530

    UI test_automation

--- tests/data_validation/Integration/page_objects/manage_articles_object.py
+++ tests/data_validation/Integration/page_objects/manage_articles_object.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from re import search
 import conf.manage_articles_locator_conf as locators
 from utils.Wrapit import Wrapit
@@ -32,59 +31,12 @@ class Managearticles_Object:
     @Wrapit._exceptionHandler
     @Wrapit._screenshot
     def click_delete_button(self):
-     add_button=self.click_element(self.delete)
-     return add_button  
+     delete_button=self.click_element(self.delete)
+     return delete_button  
 
     # click cancel button 
     @Wrapit._exceptionHandler
     @Wrapit._screenshot
     def click_cancel_button(self):
-     add_button=self.click_element(self.edit_cancel)
-     return add_button 
-            
-=======
-from re import search
-import conf.manage_articles_locator_conf as locators
-from utils.Wrapit import Wrapit
-
-class Managearticles_Object:
-    # get the manage articles locator
-    manage_article=locators.manage_article
-    # get the search locator
-    search =locators.search
-    # get the delete locator
-    delete =locators.delete
-    
-    # click manage article button
-    @Wrapit._exceptionHandler
-    @Wrapit._screenshot
-    def click_managearticle_button(self):
-     manage_button=self.click_element(self.manage_article)
-     return manage_button
-
-    # set search box
-    @Wrapit._exceptionHandler
-    @Wrapit._screenshot
-    def set_search_button(self,search):
-        result_flag = self.set_text(self.search,search)
-        self.conditional_write(result_flag,
-            positive='Set the url to: %s'%search,
-            negative='Failed to set the url in the form',
-            level='debug')
-
-    # click delete button 
-    @Wrapit._exceptionHandler
-    @Wrapit._screenshot
-    def click_delete_button(self):
-     add_button=self.click_element(self.delete)
-     return add_button  
-
-    # click cancel button 
-    @Wrapit._exceptionHandler
-    @Wrapit._screenshot
-    def click_cancel_button(self):
-     add_button=self.click_element(self.edit_cancel)
-     return add_button 
-            
->>>>>>> 4e5cbd7825e7629c1aff3910a8313c0b3082fd0b
-     
+     cancel_button=self.click_element(self.edit_cancel)
+     return cancel_button 
