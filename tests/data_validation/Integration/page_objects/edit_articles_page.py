6583e36754437253b662f9a5f33f34e5c8692fa8
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-20 12:36:06 +0530

    UI test_automation

--- tests/data_validation/Integration/page_objects/edit_articles_page.py
+++ tests/data_validation/Integration/page_objects/edit_articles_page.py
@@ -4,13 +4,23 @@ This class models the interviewscheduler_mainpage page.
 """
 from .Base_Page import Base_Page
 from .mail_object import Mail_Object
-from .edit_articles_object import Editarticles_Object
+from .search_object import Search_Object
+from .table1_object import Table_Object
 from .hamburger_object import Hamburger_Object
+from utils.Wrapit import Wrapit
 
-class Editarticles_Page(Base_Page,Mail_Object, Hamburger_Object,Editarticles_Object):
+class Editarticles_Page(Base_Page,Mail_Object, Hamburger_Object,Search_Object,Table_Object):
     "Page Object for the weather shopper main page"
 
     def start(self):
         "Use this method to go to specific URL -- if needed"
         url = 'manage-articles'
         self.open(url)
+
+    @Wrapit._exceptionHandler
+    @Wrapit._screenshot
+    def delete_article(self,search):
+        "Submit the article"
+        result_flag = self.click_hamburger_button()
+        result_flag = self.click_managearticle_button()
+        result_flag = self.set_search(search)  
