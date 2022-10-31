0d82bbf93c9876bda2ebcc6eef0095bd53a91cb7
Archana-GS <110374029+Archana-GS@users.noreply.github.com>
2022-10-31 08:35:39 +0530

    Updated

--- tests/Integration/conf/api_example_conf.py
+++ tests/Integration/conf/api_example_conf.py
@@ -1,16 +1,12 @@
 import time
 
-<<<<<<< HEAD
 # Setting current timestamp
 current_timestamp =str(int(time.time()))
 
-# Article details
-=======
 # current timestamp
 current_timestamp =str(int(time.time()))
 
 # article details
->>>>>>> 1d9d41ceaf563d25e6b18a04886aba136cdb0b1b
 article_url = 'https://www.tutorialspoint.com/software_testing_dictionary/accessibility_test.htm'
 article_title = 'Automation Test Title'
 article_description = 'Automation Test Description'
@@ -18,11 +14,7 @@ article_id = '2'
 reading_time = '15'
 article_editors = ['Arun','Raji','Avinash','Rahul','Akkul']
 
-<<<<<<< HEAD
 # base url
-=======
-# url
->>>>>>> 1d9d41ceaf563d25e6b18a04886aba136cdb0b1b
 api_url = "http://127.0.0.1:5000"
 
 # article details
