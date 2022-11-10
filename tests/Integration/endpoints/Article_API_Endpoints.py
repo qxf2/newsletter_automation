472774dd3eb23bc6fc64471c8dc8486363090b1e
Archana-GS <110374029+Archana-GS@users.noreply.github.com>
2022-11-10 12:06:25 +0530

    API Automation Testing

--- tests/Integration/endpoints/Article_API_Endpoints.py
+++ tests/Integration/endpoints/Article_API_Endpoints.py
@@ -9,7 +9,6 @@ class Article_API_Endpoints:
         """Append API end point to base URL"""
         return self.base_url+'/api'+suffix
         
-
     def add_article(self,data,headers):
       "Adds a new article"
       url = self.articles_url('/articles')
