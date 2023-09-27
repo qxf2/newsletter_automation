a398944e6f94b4a825a227999bffbc7e8e4ab99a
akkuldn <akkul.dn@qxf2.com>
2023-09-27 19:40:56 +0530

    Modified tests by adding timeout and retry policy

--- tests/integration/tests/api_tests/test_api_multiple_articles.py
+++ tests/integration/tests/api_tests/test_api_multiple_articles.py
@@ -3,7 +3,6 @@ API TEST
 Add articles in all categories - POST request(without url_params)
 
 """
-
 import os
 import sys
 import pytest
@@ -12,6 +11,9 @@ from endpoints.API_Player import API_Player
 from conf import api_tests_conf as conf
 from conftest import interactivemode_flag
 import time
+
+# Maximum number of retries
+MAX_RETRIES = 3
 
 @pytest.mark.API
 def test_api_example(test_api_obj):
@@ -24,20 +26,38 @@ def test_api_example(test_api_obj):
         headers = conf.headers
         article_editors = conf.article_editors
 
+        # Function for adding articles with retries
+        def add_article_with_retries(article_details):
+            retries = 0
+            while retries < MAX_RETRIES:
+                response = test_api_obj.add_article(article_details=article_details, headers=headers)
+                if 'Record' in response.get('response', {}).get('message', 'fail'):
+                    return True, response
+                else:
+                    retries += 1
+                    time.sleep(1)  # Wait for 1 second before retrying
+            return False, None
+
         # add articles
-        for counter,editor in enumerate(article_editors):
-            current_timestamp =str(int(time.time())+counter)
+        for counter, editor in enumerate(article_editors):
+            current_timestamp = str(int(time.time()) + counter)
             counter += 1
             StrCounter = str(counter)
-            article_details = {'url':conf.article_url +current_timestamp,'title':conf.article_title+StrCounter,'description':conf.article_description+StrCounter,'category_id':StrCounter,'article_editor':editor}
-            response = test_api_obj.add_article(article_details=article_details,
-                                                headers=headers)
+            article_details = {'url': conf.article_url + current_timestamp,
+                               'title': conf.article_title + StrCounter,
+                               'description': conf.article_description + StrCounter,
+                               'category_id': StrCounter,
+                               'article_editor': editor}
 
-            result_flag = True if 'Record' in response.get('response',{}).get('message','fail') else False
-            test_api_obj.log_result(result_flag,
-                                    positive='Successfully added new article with details %s' % response,
-                                    negative='Could not add new article with details %s' % response)   
-        
+            success, response = add_article_with_retries(article_details)
+
+            if success:
+                test_api_obj.log_result(True,
+                                        positive='Successfully added new article with details %s' % response,
+                                        negative='Could not add new article with details %s' % response)
+            else:
+                test_api_obj.log_result(False, negative='Failed to add article after retries.')
+
         # write out test summary
         expected_pass = test_api_obj.total
         actual_pass = test_api_obj.passed
@@ -49,7 +69,7 @@ def test_api_example(test_api_obj):
         test_api_obj.write("Python says:%s" % str(e))
 
     # Assertion
-    assert expected_pass == actual_pass,"Test failed: %s"%__file__
+    assert expected_pass == actual_pass, "Test failed: %s" % __file__
 
 if __name__ == '__main__':
     test_api_example()
