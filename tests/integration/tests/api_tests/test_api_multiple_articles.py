6857a9e5a932feaeefaf53a177b659d3371a840f
akkuldn <akkul.dn@qxf2.com>
2023-09-29 19:33:00 +0530

    Added temporary fix with time.sleep after the response

--- tests/integration/tests/api_tests/test_api_multiple_articles.py
+++ tests/integration/tests/api_tests/test_api_multiple_articles.py
@@ -23,12 +23,10 @@ def test_api_example(test_api_obj):
             current_timestamp =str(int(time.time())+counter)
             counter += 1
             StrCounter = str(counter)
-            test_api_obj.write("Debugging for CI")
-            time.sleep(3)
             article_details = {'url':conf.article_url +current_timestamp,'title':conf.article_title+StrCounter,'description':conf.article_description+StrCounter,'category_id':StrCounter,'article_editor':editor}
             response = test_api_obj.add_article(article_details=article_details,
                                                 headers=headers)
-
+            time.sleep(3)
             result_flag = True if 'Record' in response.get('response',{}).get('message','fail') else False
             test_api_obj.log_result(result_flag,
                                     positive='Successfully added new article with details %s' % response,
