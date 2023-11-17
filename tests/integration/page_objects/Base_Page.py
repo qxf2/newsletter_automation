bcb80bf5e30d2c28d3fd7cc4c1922a24f8d6cf07
ajitava <ajitava.deb@qxf2.com>
2023-11-17 21:39:10 +0530

    Added accessibility test for all pages and a method to return pages in page factory

--- tests/integration/page_objects/Base_Page.py
+++ tests/integration/page_objects/Base_Page.py
@@ -39,7 +39,7 @@ class Borg:
 
         return result_flag
 
-class Base_Page(Borg,unittest.TestCase):
+class Base_Page(Borg,unittest.TestCase, Accessibility_Object):
     "Page class that all page models can inherit from"
 
     def __init__(self,base_url):
