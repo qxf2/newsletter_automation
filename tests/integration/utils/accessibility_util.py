c3986713f15a56dee0a5407924c9c35c262ca92b
ajitava <ajitava.deb@qxf2.com>
2023-11-23 19:46:36 +0530

    Have done a wrapper in base page which uses the methods from accessibility_util module and have moved back the axe_min.js to util

--- tests/integration/utils/accessibility_util.py
+++ tests/integration/utils/accessibility_util.py
@@ -8,22 +8,18 @@ This is a class object that contains the following methods
 import os
 from axe_selenium_python import Axe
 
-class Accessibility_Util:
-    "Methods to inject andrun Axe"
-    axe_injected = False
+def inject_accessibility_test(self, driver):
+    "Inject Axe in the test"
+    script_url=os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "utils", "axe.min.js"))
+    self.axe = Axe(driver)
+    self.axe.inject()
 
-    def inject_accessibility_test(self):
-        "Inject Axe in the test"
-        script_url=os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "..", "conf", "axe.min.js"))
-        self.axe = Axe(self.driver)
-        self.axe.inject()
+def run_accessibility_test(self):
+    "Run Axe in the test"
+    return self.axe.run()
 
-    def run_accessibility_test(self):
-        "Run Axe in the test"
-        return self.axe.run()
-
-    def write_accessibility_test(self, result, file):
-        "Write the accessibility test to a file"
-        return self.axe.write_results(result, file) 
+def write_accessibility_test(self, result, file):
+    "Write the accessibility test to a file"
+    return self.axe.write_results(result, file)
    
