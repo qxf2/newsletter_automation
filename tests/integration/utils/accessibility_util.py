ae59e0045a78e9036b0fcf0be0684cd31fbd5451
ajitava <ajitava.deb@qxf2.com>
2023-11-29 18:09:04 +0530

    Added script_url as param and static method to the page factory methods

--- tests/integration/utils/accessibility_util.py
+++ tests/integration/utils/accessibility_util.py
@@ -6,16 +6,17 @@ This is a module that contains the following methods
 
 import os
 from axe_selenium_python import Axe
+script_url=os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "utils", "axe.min.js"))
 
 def inject_accessibility_test(driver):
     "Inject Axe in the test"
-    script_url=os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..", "utils", "axe.min.js"))
-    axe = Axe(driver)
+    # script_url=os.path.abspath(
+    #     os.path.join(os.path.dirname(__file__), "..", "utils", "axe.min.js"))
+    axe = Axe(driver, script_url)
     axe.inject()
 
 def run_accessibility_test(driver):
     "Run Axe in the test"
-    axe = Axe(driver)
+    axe = Axe(driver, script_url)
     return axe.run()
-   
