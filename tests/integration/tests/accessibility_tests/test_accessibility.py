d58b358e8e39029887c91d467685009f41dd9ab8
ajitava <ajitava.deb@qxf2.com>
2023-12-22 13:51:22 +0530

    Added result summary in accessibility test

--- tests/integration/tests/accessibility_tests/test_accessibility.py
+++ tests/integration/tests/accessibility_tests/test_accessibility.py
@@ -25,33 +25,48 @@ search = conf.search
 @pytest.mark.ACCESSIBILITY
 def test_accessibility(test_obj, snapshot):
     "Inject Axe and create snapshot for every page"
+    try:
 
-    #Get all pages
-    page_names = PageFactory.get_all_page_names()
+        #Initalize flags for tests summary
+        expected_pass = 0
+        actual_pass = -1        
+        #Get all pages
+        page_names = PageFactory.get_all_page_names()
 
-    for page in page_names:
-        test_obj = PageFactory.get_page_object(page,base_url=test_obj.base_url)
-        if page == "edit articles page":
-            #Set the search string
-            test_obj.search_word(search)
-            #Click the edit button
-            test_obj.edit_articles(url,title,description,runtime,category)
-        #Inject Axe
-        test_obj.accessibility_inject_axe()
-        #Run Axe in every page
-        result = test_obj.accessibility_run_axe()
-        #Serialize dict to JSON-formatted string
-        result_str = json.dumps(result, ensure_ascii=False, separators=(',', ':'))
-        #Formatting result by removing \n,\\,timestamp
-        #Every run will have a different timestamp.
-        cleaned_result = re.sub(r'\\|\n|\r|"timestamp":\s*"[^"]*"', '', result_str)
-        ##Add pages here which needs formatting before creating snapshot
-        #Formatting add article page
-        if page == "add articles page":
-            cleaned_result = re.sub(r'name="csrf_token" value="[^"]*"', '', cleaned_result)
-        #Formatting create newsletter page
-        if page == "create newsletter page":
-            cleaned_result = re.sub(r'name="csrf_token" type="hidden" value="[^"]*"', '',
-                                    cleaned_result)
-        #Create Snapshot
-        snapshot.assert_match(f"{cleaned_result}", f'snapshot_output_{page}.txt')
+        for page in page_names:
+            test_obj = PageFactory.get_page_object(page,base_url=test_obj.base_url)
+            if page == "edit articles page":
+                #Set the search string
+                test_obj.search_word(search)
+                #Click the edit button
+                test_obj.edit_articles(url,title,description,runtime,category)
+            #Inject Axe
+            test_obj.accessibility_inject_axe()
+            #Run Axe in every page
+            result = test_obj.accessibility_run_axe()
+            #Serialize dict to JSON-formatted string
+            result_str = json.dumps(result, ensure_ascii=False, separators=(',', ':'))
+            #Formatting result by removing \n,\\,timestamp
+            #Every run will have a different timestamp.
+            cleaned_result = re.sub(r'\\|\n|\r|"timestamp":\s*"[^"]*"', '', result_str)
+            ##Add pages here which needs formatting before creating snapshot
+            #Formatting add article page
+            if page == "add articles page":
+                cleaned_result = re.sub(r'name="csrf_token" value="[^"]*"', '', cleaned_result)
+            #Formatting create newsletter page
+            if page == "create newsletter page":
+                cleaned_result = re.sub(r'name="csrf_token" type="hidden" value="[^"]*"', '',
+                                        cleaned_result)
+            #Create Snapshot
+            snapshot.assert_match(f"{cleaned_result}", f'snapshot_output_{page}.txt')
+
+        #Print out the result
+        test_obj.write_test_summary()
+        expected_pass = test_obj.result_counter
+        actual_pass = test_obj.pass_counter
+
+    except Exception as e:
+        print("Exception when trying to run test: %s"%__file__)
+        print("Python says:%s"%str(e))
+
+    assert expected_pass == actual_pass, "Test failed: %s"%__file__            
