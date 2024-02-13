a9eaae044840c5758d99da57a6d683703e25eb14
ajitava <ajitava.deb@qxf2.com>
2024-02-13 13:24:13 +0530

    Masked bgcolors in create newsletter

--- tests/integration/tests/accessibility_tests/test_accessibility.py
+++ tests/integration/tests/accessibility_tests/test_accessibility.py
@@ -52,13 +52,20 @@ def test_accessibility(test_obj):
                 cleaned_result = re.sub(r'{"html":"","target":.*', '{"html":"","target":', cleaned_result)
             if page == "create newsletter page":
                 #removing csrf_token from create newsletter page
+                cleaned_result = re.sub(r'("bgColor":"#[\da-fA-F]{6}"|"contrastRatio":\d+\.\d+|"expectedContrastRatio":"[\d\.]+:1"|"message":"Element has sufficient color contrast of \d+")', '', cleaned_result)
                 cleaned_result = re.sub(r'name\s*=\s*"csrf_token"(?:\s*type\s*=\s*"hidden")?\s*value\s*=\s*"[^"]*"', '', cleaned_result)             
 
             # Create a filename based on the page name
             filename = f'{page}_output.txt'
             print(filename)
             print(cleaned_result)
-            
+                
+            # filename = f'{page}_output.txt'
+
+            # # # # Open the file in write mode
+            # with open(filename, 'w', encoding='utf-8') as file:
+            #     file.write(cleaned_result)                 
+
             #Compare Snapshot for each page
             snapshot_result = test_obj.snapshot_assert_match(f"{cleaned_result}",
                                                              f'snapshot_output_{page}.txt')
