95ad7e5f0a7b79992bdbc9d4474cd8d74842d427
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2024-02-21 19:07:35 +0530

    comments modified

--- terraform/terraform-aws-newsletter-automation/output.tf
+++ terraform/terraform-aws-newsletter-automation/output.tf
@@ -1,4 +1,4 @@
-# Terraform Output Values
+# Terraform Output Values. This output values are useful in chef InSpec tests.
 output "instance_publicip" {
   description = "EC2 Instance Public IP"
   value       = aws_instance.newsletter_instance.public_ip
@@ -9,9 +9,11 @@ output "instance_publicdns" {
   value       = aws_instance.newsletter_instance.public_dns
 }
 output "lambda_function_arn" {
+  description = "Lambda ARN"
   value = aws_lambda_function.newsletter_lambda.arn
 }
 
 output "aws_sqs_queue" {
+  description = "SQS ARN"
   value = aws_sqs_queue.newsletter_sqs.arn
 }
