9140241dbb269ab75ba06ced2f514040c0e472f1
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2024-05-30 16:55:38 +0530

    reportportal issues similar to fixed in page object model

--- tests/integration/conf/base_url_conf.py
+++ tests/integration/conf/base_url_conf.py
@@ -1,4 +1,5 @@
 """
 Conf file for base_url
 """
-base_url = "https://staging-newsletter-generator.qxf2.com/"
+#base_url = "https://staging-newsletter-generator.qxf2.com/"
+base_url   = "http://localhost:5000"
--- tests/integration/conf/report_portal_conf.py
+++ tests/integration/conf/report_portal_conf.py
@@ -3,7 +3,8 @@ To run the test in reportportal we need UUID and Endpoint.
 We are stroing these two values here
 """
 
-report_portal_uuid = "Enter your UUID here"
+#report_portal_uuid = "Enter your UUID here"
+report_portal_api_key = "Enter your report portal api key here"
 report_portal_endpoint = "Enter your endpoint here"
 report_portal_project = "Enter your Project here"
 report_portal_launch = "Enter your project launch here"
