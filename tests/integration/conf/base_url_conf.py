cb6dc9b6f1fe7e0656d1a6599b67e46aea043672
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2024-06-05 19:39:07 +0530

    changed from local host URL to staging-newsletter URL

--- tests/integration/conf/base_url_conf.py
+++ tests/integration/conf/base_url_conf.py
@@ -1,5 +1,4 @@
 """
 Conf file for base_url
 """
-#base_url = "https://staging-newsletter-generator.qxf2.com/"
-base_url   = "http://localhost:5000"
+base_url = "https://staging-newsletter-generator.qxf2.com/"
